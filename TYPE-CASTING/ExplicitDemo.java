7fd2aaaded3dabaf17c5d6728bdc26362c5cd6d2
Abhishek Gupta <abhishekgupta0301@gmail.com>
2023-12-12 00:11:45 +0530

    java

---
+++ TYPE-CASTING/ExplicitDemo.java
@@ -0,0 +1,13 @@
+class ExplicitDemo{
+  public static void main(String[] args) {
+    byte a;
+    int x=350;
+    double d =123.456;
+    a=(byte)x;
+    System.out.println("a="+a);
+    x=(int)d;
+
+    System.out.println("x="+x);
+    System.out.println("complete");
+  }
+}
