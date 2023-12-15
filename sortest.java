429a36919dd8f49457d7922937f02845e2cf603f
Abhishek Gupta <abhishekgupta0301@gmail.com>
2023-12-16 01:28:21 +0530

    only i did one program today sorry for late starting

---
+++ sortest.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class sortest {
+  public static void main(String[] args) {
+
+    int n, i;
+    Scanner s = new Scanner(System.in);
+    System.out.println("enter the size of the array ");
+    n = s.nextInt();
+    int arr[] = new int[n];
+    for (i = 0; i < n; i++) {
+      System.out.println("enter the elements in the array");
+      arr[i] = s.nextInt();
+
+    }
+
+    for (i = 0; i < n; i++) {
+      if(arr[i]<arr[0]){
+      arr[0] = arr[i];
+      }
+    }
+    System.out.println("smallest="+arr[0]);
+
+  }
+
+}
