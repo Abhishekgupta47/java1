1c154e6ce7319e21fde3caca0ebe90858a830952
Abhishek Gupta <abhishekgupta0301@gmail.com>
2023-12-21 23:33:44 +0530

    erroe in code

--- ArmStrong.java
+++ ArmStrong.java
@@ -3,17 +3,18 @@ public class ArmStrong {
   public static void main(String[] args) {
     int n,r,sum=0;
     int t;
-    t=n;
+    
     System.out.println("enter the number");
     Scanner s=new Scanner(System.in);
      n=s.nextInt();
+     t=n;
      while(n>0){
       r=n%10;
       sum=sum+(r+r+r);
       n=n/10;
 
      }
-     if(t==sum){
+     if(sum==t){
       System.out.println("\nArmstrong");
      }
      else{
