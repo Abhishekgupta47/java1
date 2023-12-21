7894a50471cde50f9b735404cf3ddd47049571c1
Abhishek Gupta <abhishekgupta0301@gmail.com>
2023-12-21 23:26:44 +0530

    working on it

--- ArmStrong.java
+++ ArmStrong.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
 public class ArmStrong {
-  
+  public static void main(String[] args) {
+    int n,r,sum=0;
+    int t;
+    t=n;
+    System.out.println("enter the number");
+    Scanner s=new Scanner(System.in);
+     n=s.nextInt();
+     while(n>0){
+      r=n%10;
+      sum=sum+(r+r+r);
+      n=n/10;
+
+     }
+     if(t==sum){
+      System.out.println("\nArmstrong");
+     }
+     else{
+      System.out.println("not armstrong");
+     }
+
+  }
 }
