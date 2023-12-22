283b2e772881350d8ed5684e7606c36a22cdc43f
Abhishek Gupta <abhishekgupta0301@gmail.com>
2023-12-23 00:20:21 +0530

    ?

--- class/Fact.java
+++ class/Fact.java
@@ -12,12 +12,12 @@ public class Fact {
     }
 
     public static void main(String[] args) {
-        int x;
+        int a;
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the number");
-        x=sc.nextInt();
+        a=sc.nextInt();
         Fact f =new Fact();
-        f.factorial(x);
+        f.factorial(a);
        
      } 
     }
