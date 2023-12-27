23523455e4a12fe2f3fba670a0b6ef3e0eb67bd1
Abhishek Gupta <abhishekgupta0301@gmail.com>
2023-12-27 22:59:56 +0530

    class Squre inherits the property of class Number

--- Inheritance/multipleInheritence.java
+++ Inheritance/multipleInheritence.java
@@ -1,7 +1,17 @@
-
-public class multipleInheritence {
-  public static void main(String[] args) {
-    
+class Number{
+  int num;
+  void getval(int n)
+  {
+    num=n;
   }
-  
-}
+    void showval(){
+      System.out.println("num="+num);
+    }
+  }
+  class Square extends Number{
+    int sq;
+    void calcsquare()
+    {
+      sq=num*num;
+    }
+  }
