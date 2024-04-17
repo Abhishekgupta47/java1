ee0760342a730c1d5f60273664adbfaec9da7ef7
Abhishek Gupta <abhishekgupta0301@gmail.com>
2024-04-18 00:02:56 +0530

    working on

--- ARRAY/peak.java
+++ ARRAY/peak.java
@@ -1,16 +1,33 @@
 import java.util.Scanner;
+
 public class peak {
 
   public static void main(String[] args) {
-    int size ,i;
+    int size, i;
     Scanner s = new Scanner(System.in);
-   System.out.println("enter the array size");
-   size=s.nextInt();
-   int arr[]=new int[size];
-   for(i=0;i<size;i++){
-    System.out.println("enter the elementd in array");
-    arr[i]=s.nextInt();
-   }
+    System.out.println("enter the array size");
+    size = s.nextInt();
+    int arr[] = new int[size];
+    for (i = 0; i < size; i++) {
+      System.out.println("enter the elementd in array");
+      arr[i] = s.nextInt();
+    }
+    System.out.println("the elements in array are ");
+    for (i = 0; i < size; i++) {
+      System.out.println(arr[i]);
+
+    }
+    System.out.println("index of a peak point is ",findPeak(arr,size));
+
   }
+
+
+}
+findPeak(arr[],size){
+  if(n==1)
+
+    return 0;
+
   
+
 }
---
+++ pettern/pert1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class pert1{
+  public static void main(String[] args) {
+    
+    int n =4;
+    int m=5;
+    //outer loop
+    for(int i=1;i<=n;i++){
+      // inne loop
+      for(int j=1;j<=5;j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
