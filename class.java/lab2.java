80a89cb5983084c35bc0959dd614c647c88dc7fd
Abhishek Gupta <abhishekgupta0301@gmail.com>
2024-05-02 12:30:29 +0530

    table of count

--- class.java/lab2.java
+++ class.java/lab2.java
@@ -5,16 +5,20 @@ class Student {
   public Student() {
       count++;
   }
-  // public static int getCount() {
-  //     return count;
-  // }
+  
 }
 
 public class lab2 {
   public static void main(String[] args) {
-      
+      int table;
       Student student1 = new Student();
       Student student2 = new Student();
-      System.out.println("Number of Student objects: " + Student.count); // Output: 3
+      System.out.println("Number of Student objects: " + Student.count);// for counting the object count
+      System.out.println("table of count"); 
+      for(int i=1;i<=10;i++){
+        table=Student.count*i;
+        System.out.println(Student.count+"*"+i+"="+table);
+        
+      }
   }
 }
